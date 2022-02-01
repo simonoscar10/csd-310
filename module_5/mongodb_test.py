b0890070a775aa50b45c4b96343caa482df1d9dd
simonoscar10 <35694401+simonoscar10@users.noreply.github.com>
2022-02-01 17:56:07 -0600

    Delete mongodb_test.py

--- module_5/mongodb_test.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-url = "mongodb+srv://admin:admin@cluster0.4jiwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-client =  MongoClient(url)
-db = client.pytech
-print(db.list_collection_names())
