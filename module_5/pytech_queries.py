d8b75cfb79faf7907a3ea56a9feb74ba88f6a604
simonoscar10 <simonoscar10@yahoo.com>
2022-02-01 19:01:38 -0600

    pytech insert commit

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,11 @@
+import pymongo
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.4jiwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+cluster =  MongoClient(url)
+db = cluster["pytech"]
+students = db["students"]
+
+docs = students.find({})
+for doc in docs:
+    print(doc)
