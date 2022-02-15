fa6075195e294472be9668d854261d028e9310cc
simonoscar10 <35694401+simonoscar10@users.noreply.github.com>
2022-02-15 08:47:36 -0600

    new commit

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 config = {
     "user": "pysports_user",
-    "password": "MySQL8IsGreat!",
+    "password": "password",
     "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
